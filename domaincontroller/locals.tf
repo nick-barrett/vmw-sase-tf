3632f77463669d89ab172717d28f6a8a8593bb10
Nick Barrett <nbarrett@nbarrett-a01.vmware.com>
2021-08-26 13:14:43 -0500

    NSG Rule Cleanup
    - The DC NSG will be long
    - Replaced this with an allow-all for now

--- domaincontroller/locals.tf
+++ domaincontroller/locals.tf
@@ -11,41 +11,5 @@ locals {
       source_address_prefix      = "*"
       destination_address_prefix = "*"
     }
-
-    ldap = {
-      name                       = "LDAP"
-      priority                   = 110
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "389"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-
-    rdp1 = {
-      name                       = "RDP1"
-      priority                   = 200
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "3389"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
-
-    rdp2 = {
-      name                       = "RDP2"
-      priority                   = 300
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Udp"
-      source_port_range          = "*"
-      destination_port_range     = "3389"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
   }
 }
